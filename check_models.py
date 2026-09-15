19ecd667220dbdb394e964015de6f790d9cfb62e
DjenyGal <dzenifergal@gmail.com>
2026-09-15 23:42:25 +0300

    бран check_models.py из репозитория (содержал секретный ключ)

--- check_models.py
+++
@@ -1,9 +0,0 @@
-from gigachat import GigaChat
-
-GIGACHAT_TOKEN = "MDE5ZmQ4OGItYzdjOC03N2JiLWFlZmEtZDA1NDU4NzBmZTg3OmFmYjE4MzY1LTBmMWYtNGJmOC1iNTJhLThlMTE2NjQyMDUxMQ=="
-
-giga = GigaChat(credentials=GIGACHAT_TOKEN, verify_ssl_certs=False)
-
-models = giga.get_models()
-for model in models.data:
-    print(model.id_)
